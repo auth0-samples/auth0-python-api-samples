298aa245a94a41c188622a28ca0f1d066594e10c
Alexis Luque <aluque@indexlabs.com.uy>
2017-09-05 16:08:12 -0300

    Fix indent in server.py

--- 00-Starter-Seed/server.py
+++ 00-Starter-Seed/server.py
@@ -40,23 +40,24 @@ def get_token_auth_header():
     auth = request.headers.get("Authorization", None)
     if not auth:
         raise AuthError({"code": "authorization_header_missing",
-                             "description":
-                                 "Authorization header is expected"}, 401)
+                        "description":
+                            "Authorization header is expected"}, 401)
 
     parts = auth.split()
 
     if parts[0].lower() != "bearer":
         raise AuthError({"code": "invalid_header",
-                             "description":
-                                 "Authorization header must start with"
-                                 "Bearer"}, 401)
+                        "description":
+                            "Authorization header must start with"
+                            " Bearer"}, 401)
     elif len(parts) == 1:
         raise AuthError({"code": "invalid_header",
-                             "description": "Token not found"}, 401)
+                        "description": "Token not found"}, 401)
     elif len(parts) > 2:
         raise AuthError({"code": "invalid_header",
-                             "description": "Authorization header must be"
-                                            " Bearer token"}, 401)
+                        "description":
+                            "Authorization header must be"
+                            " Bearer token"}, 401)
 
     token = parts[1]
     return token
@@ -88,12 +89,14 @@ def requires_auth(f):
             unverified_header = jwt.get_unverified_header(token)
         except jwt.JWTError:
             raise AuthError({"code": "invalid_header",
-                                 "description": "Invalid header. "
-                                                "Use an RS256 signed JWT Access Token"}, 401)
+                            "description":
+                                "Invalid header. "
+                                "Use an RS256 signed JWT Access Token"}, 401)
         if unverified_header["alg"] == "HS256":
             raise AuthError({"code": "invalid_header",
-                                 "description": "Invalid header. "
-                                                "Use an RS256 signed JWT Access Token"}, 401)
+                            "description":
+                                "Invalid header. "
+                                "Use an RS256 signed JWT Access Token"}, 401)
         rsa_key = {}
         for key in jwks["keys"]:
             if key["kid"] == unverified_header["kid"]:
@@ -115,20 +118,22 @@ def requires_auth(f):
                 )
             except jwt.ExpiredSignatureError:
                 raise AuthError({"code": "token_expired",
-                                     "description": "token is expired"}, 401)
+                                "description": "token is expired"}, 401)
             except jwt.JWTClaimsError:
                 raise AuthError({"code": "invalid_claims",
-                                     "description": "incorrect claims,"
-                                                    " please check the audience and issuer"}, 401)
+                                "description":
+                                    "incorrect claims,"
+                                    " please check the audience and issuer"}, 401)
             except Exception:
                 raise AuthError({"code": "invalid_header",
-                                     "description": "Unable to parse authentication"
-                                                    " token."}, 400)
+                                "description":
+                                    "Unable to parse authentication"
+                                    " token."}, 400)
 
             _app_ctx_stack.top.current_user = payload
             return f(*args, **kwargs)
         raise AuthError({"code": "invalid_header",
-                             "description": "Unable to find appropriate key"}, 400)
+                        "description": "Unable to find appropriate key"}, 400)
     return decorated
 
 
