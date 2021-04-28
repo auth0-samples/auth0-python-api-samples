6aa1e3a3c04edc9dd2ecc8082cc2b6d6267770e1
Jake Kaplan <jakkap@stanford.edu>
2021-04-28 16:44:22 -0700

    Simplify logic for unpacking auth header

--- 00-Starter-Seed/server.py
+++ 00-Starter-Seed/server.py
@@ -43,23 +43,14 @@ def get_token_auth_header():
                         "description":
                             "Authorization header is expected"}, 401)
 
-    parts = auth.split()
-
-    if parts[0].lower() != "bearer":
+    try:
+        bearer, token = auth.split()
+        assert bearer.lower() == "bearer"
+    except (ValueError, AssertionError):
         raise AuthError({"code": "invalid_header",
                         "description":
-                            "Authorization header must start with"
-                            " Bearer"}, 401)
-    elif len(parts) == 1:
-        raise AuthError({"code": "invalid_header",
-                        "description": "Token not found"}, 401)
-    elif len(parts) > 2:
-        raise AuthError({"code": "invalid_header",
-                        "description":
-                            "Authorization header must be"
-                            " Bearer token"}, 401)
+                            "Invalid or missing bearer token"}, 401)
 
-    token = parts[1]
     return token
 
 
